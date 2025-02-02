b74b0a62b33a5f17022e7b4409b5a8b195c8dd62
oben-desmond <obend678@gmail.com>
2025-02-02 17:17:24 +0100

    added models to support data in csv

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# To be defined here
+class Person(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=20)
+    region = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return self.id
+    
